307fea34ac0afe291d6a940701308f4c66707988
zulkufadsiz <zulkufadsiz@gmail.com>
2026-02-21 21:39:53 +0100

    feat(android): add native React module for IMA ads playback

---
+++ android/app/src/main/java/com/mamoplayer/ads/MamoAdsPackage.kt
@@ -0,0 +1,16 @@
+package com.mamoplayer.ads
+
+import com.facebook.react.ReactPackage
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.uimanager.ViewManager
+
+class MamoAdsPackage : ReactPackage {
+  override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
+    return listOf(MamoAdsModule(reactContext))
+  }
+
+  override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
+    return emptyList()
+  }
+}
--- android/app/src/main/java/com/mamoplayerandroid/MainApplication.kt
+++ android/app/src/main/java/com/mamoplayerandroid/MainApplication.kt
@@ -7,6 +7,7 @@ import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
+import com.mamoplayer.ads.MamoAdsPackage
 
 class MainApplication : Application(), ReactApplication {
 
@@ -15,7 +16,7 @@ class MainApplication : Application(), ReactApplication {
       override fun getPackages(): List<ReactPackage> =
         PackageList(this).packages.apply {
           // Packages that cannot be autolinked yet can be added manually here, for example:
-          // add(MyReactNativePackage())
+          add(MamoAdsPackage())
         }
 
       override fun getJSMainModuleName(): String = "index"
