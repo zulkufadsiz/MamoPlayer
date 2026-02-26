fa00e933502c26b16ec4f8e7e0ba80dcd2558c6b
zulkufadsiz <zulkufadsiz@gmail.com>
2026-02-26 20:15:33 +0100

    feat(core): add reusable Timeline component with scrub seek support

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,15 +1,16 @@
+export { Timeline, type TimelineProps } from './components/Timeline';
 export {
-  MamoPlayerCore as MamoPlayer,
-  MamoPlayerCore,
-  type MamoPlayerCoreProps,
-  type MamoPlayerCoreProps as MamoPlayerProps,
-  type MamoPlayerSource,
+    MamoPlayerCore as MamoPlayer,
+    MamoPlayerCore,
+    type MamoPlayerCoreProps,
+    type MamoPlayerCoreProps as MamoPlayerProps,
+    type MamoPlayerSource
 } from './MamoPlayer';
 export type {
-  PlaybackEvent,
-  PlaybackEventBase,
-  PlaybackEventType,
-  PlaybackTimePayload,
+    PlaybackEvent,
+    PlaybackEventBase,
+    PlaybackEventType,
+    PlaybackTimePayload
 } from './types/playback';
 
 export { default } from './MamoPlayer';
